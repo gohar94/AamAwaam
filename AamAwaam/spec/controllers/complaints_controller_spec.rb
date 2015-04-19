865bc4b869779fc4cfc82845b00dbfb4d7b0bf2e
Gohar Irfan <goharirfan94@gmail.com>
2015-04-19 17:52:58 +0500

    complaints controller. rspec for complaints started.

---
+++ AamAwaam/spec/controllers/complaints_controller_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe ComplaintsController, type: :controller do
+
+	describe 'add complaints using a form' do
+		it 'should render the form to add complaints'
+		it 'should submit the form to the controller and should add new complain'
+		it 'should display success message and show page with details of the newcomplain'
+	end
+
+end
