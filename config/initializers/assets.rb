8998ce50388d5c9f1b27d7efd088e2fbac03f56e
Mohammad <mlaghari1993@gmail.com>
2015-04-29 14:19:47 -0700

    front-end

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -2,6 +2,7 @@
 
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
+Rails.application.config.assets.precompile += %w( bootstrap.css styles_new.css bootstrap.js scripts_new.js)
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
